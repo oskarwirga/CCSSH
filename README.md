00c4d5b732ad991a871e7020a29fb8bb6dcbaa1b
oskarw <oskar@wirga.com>
2018-03-29 19:44:17 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Control Center SSH Toggle
+I sure do love SSH, but I love security more. Here's an easy Control Center toggle for SSH. If you really don't like Control Center, you can also manually enable it in Settings. 
---
+++ ccssh/OW2RootListController.m
@@ -0,0 +1,13 @@
+#include "OW2RootListController.h"
+
+@implementation OW2RootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
