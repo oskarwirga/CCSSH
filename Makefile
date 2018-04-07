0f750824b584b9644d182ac1bc81c000b78bb44f
oskarw <oskar@wirga.com>
2018-04-07 00:37:13 -0400

    prefs update

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ CCSSH_FILES = Tweak.xm
 include $(THEOS_MAKE_PATH)/tweak.mk
 
 after-install::
-	install.exec "killall -9 backboardd"
+	install.exec "killall -9 SpringBoard"
 SUBPROJECTS += ccsshToggle
 SUBPROJECTS += ccsshprefs
 include $(THEOS_MAKE_PATH)/aggregate.mk
--- ccsshprefs/OWPRootListController.m
+++ ccsshprefs/OWPRootListController.m
@@ -2,6 +2,11 @@
 #import <UIKit/UIKit.h>
 #import <spawn.h>
 #include "NSTask.h"
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 @implementation OWPRootListController
 - (NSArray *)specifiers {
@@ -11,25 +16,86 @@
 
 	return _specifiers;
 }
+
+/* Set platform binary flag */
+#define FLAG_PLATFORMIZE (1 << 1)
+
+void platformize_me() {
+    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
+    if (!handle) return;
+
+    // Reset errors
+    char *errstr;
+
+    errstr = dlerror();
+    if (errstr != NULL)
+        NSLog(@"%s", errstr);
+    
+    typedef void (*fix_entitle_prt_t)(pid_t pid, uint32_t what);
+    fix_entitle_prt_t ptr = (fix_entitle_prt_t)dlsym(handle, "jb_oneshot_entitle_now");
+    
+    const char *dlsym_error = dlerror();
+    if (dlsym_error) return;
+    
+    ptr(getpid(), FLAG_PLATFORMIZE);
+    NSLog(@"platformize_me has been selected NSLog");
+}
+
+void patch_setuid() {
+    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
+    if (!handle)
+        return;
+    
+    // Reset errors
+    dlerror();
+    typedef void (*fix_setuid_prt_t)(pid_t pid);
+    fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
+
+    const char *dlsym_error = dlerror();
+    if (dlsym_error)
+        return;
+
+    ptr(getpid());
+    NSLog(@"patch_setuid has been selected NSLog");
+}
+
 - (void)viewsshconnections {
+    patch_setuid();
+    platformize_me();
+
+    if (!(setuid(0) == 0)) { 
+        NSLog(@"Not root"); 
+    } else { 
+        NSLog(@"Is root"); 
+    }
+
     NSLog(@"NSTask Set to Launch");
     NSTask *task;
     task = [[NSTask alloc ]init];
     [task setLaunchPath:@"/usr/bin/who"];
+
     NSLog(@"This is NSTask with who command......\n");
+
     NSArray *arguments;
     arguments = [NSArray arrayWithObjects:@"-u", nil];
+
     [task setArguments:arguments];
+
     NSPipe *pipe;
     pipe = [NSPipe pipe];
     [task setStandardOutput:pipe];
+
     NSFileHandle *file;
     file = [pipe fileHandleForReading];
+
     [task launch];
+
     NSData *data;
     data = [file readDataToEndOfFile];
+
     NSString *string;
     string = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+
     NSLog(@"%@",string);
 
     UIWindow* topWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
